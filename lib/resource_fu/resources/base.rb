547c441fa7881e6011b9007597e98b2838b95910
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2013-04-14 11:11:54 +0400

    Add implicit module declaration

--- lib/resource_fu/resources/base.rb
+++ lib/resource_fu/resources/base.rb
@@ -3,7 +3,7 @@
 # Example:
 #
 # class TestResource < ResourceFu::Resources::Base
-#   
+#
 #   resource :read do |id, options = {}|
 #     resource :post do
 #       Post.find(id)
@@ -21,69 +21,71 @@
 # result = resource.read
 # result.resource(:post)
 
-module ResourceFu::Resources
-  class Configuration
-    attr_reader :actions
+module ResourceFu
+  module Resources
+    class Configuration
+      attr_reader :actions
 
-    def initialize
-      @actions = []
-    end
-  end
-  
-  class Resource
-    attr_reader :resources
-    delegate    :count, :size, :each, :all, :map,
-                :inject, :reject, :collect, to: :resources
-
-    def initialize
-      @resources = {}
-    end
-
-    def resource(name)
-      @resources[name]
-    end
-    
-    def add(name, resource, options = {})
-      @resources[name] = resource
-      self
-    end
-
-    def remove(name)
-      @resources.delete(name)
-    end
-  end
-  
-  class Base
-
-    class_attribute :configuration
-    attr_accessor   :accessor
-    attr_reader     :resource_options, :resource_result
-
-    def initialize(options = {})
-      @resource_options = options.dup
-      @accessor         = @resource_options.delete(:as)
-      @resource_result  = Resource.new
-    end
-
-    class << self
-      def describe_resource(name, &block)
-        raise ArgumentError, "Block is required" if !block_given?
-        self.configuration ||=  Configuration.new
-        configuration.actions << name
-        define_method name do |*args|
-          instance_exec *args, &block
-          resource_result
-        end
+      def initialize
+        @actions = []
       end
     end
 
-    private
+    class Resource
+      attr_reader :resources
+      delegate    :count, :size, :each, :all, :map,
+                  :inject, :reject, :collect, to: :resources
 
-    def resource(name = :default, options = {}, &block)
-      if block_given?
-        resource_result.add(name, yield)
-      else
-        resource_result.resource(name)
+      def initialize
+        @resources = {}
+      end
+
+      def resource(name)
+        @resources[name]
+      end
+
+      def add(name, resource, options = {})
+        @resources[name] = resource
+        self
+      end
+
+      def remove(name)
+        @resources.delete(name)
+      end
+    end
+
+    class Base
+
+      class_attribute :configuration
+      attr_accessor   :accessor
+      attr_reader     :resource_options, :resource_result
+
+      def initialize(options = {})
+        @resource_options = options.dup
+        @accessor         = @resource_options.delete(:as)
+        @resource_result  = Resource.new
+      end
+
+      class << self
+        def describe_resource(name, &block)
+          raise ArgumentError, "Block is required" if !block_given?
+          self.configuration ||=  Configuration.new
+          configuration.actions << name
+          define_method name do |*args|
+            instance_exec *args, &block
+            resource_result
+          end
+        end
+      end
+
+      private
+
+      def resource(name = :default, options = {}, &block)
+        if block_given?
+          resource_result.add(name, yield)
+        else
+          resource_result.resource(name)
+        end
       end
     end
   end
